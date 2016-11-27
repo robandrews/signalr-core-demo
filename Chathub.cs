9405f6518ec2a3ff9419124a256cbaeaafaf3bf4
Rob Andrews <rob.andrews@alumni.stanford.edu>
2016-11-27 17:10:47 -0500

    Initial commit

---
+++ Chathub.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+
+public class ChatHub : Hub
+    {
+        public static List<string> ConnectedUsers;
+
+        public void Send(string originatorUser, string message)
+        {
+            Clients.All.messageReceived(originatorUser, message);
+        }
+
+        public void Connect(string newUser)
+        {
+            if (ConnectedUsers == null)
+                ConnectedUsers = new List<string>();
+
+            ConnectedUsers.Add(newUser);
+            Clients.Caller.getConnectedUsers(ConnectedUsers);
+            Clients.Others.newUserAdded(newUser);
+        }
+    }
---
+++ Startup.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalRApplication
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR(options => 
+            {
+                options.Hubs.EnableDetailedErrors = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            app.UseSignalR();
+        }
+    }
+}
